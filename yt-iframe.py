1998b8fc642bcce32947eb625dcf9ea4852db658
Cyril Li <cyrils.li@icloud.com>
2026-05-28 15:23:21 -0500

    add local wisp server support

--- yt-iframe.py
+++ yt-iframe.py
@@ -1,6 +1,15 @@
+import runpy, socket, sys, threading
 from setproctitle import setproctitle
 setproctitle("yt-iframe")
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
 os.chdir(os.path.dirname(__file__))
-HTTPServer(("", 8823), SimpleHTTPRequestHandler).serve_forever()
+def start_wisp_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        if s.connect_ex(("localhost", 9477)) == 0:
+            return
+    sys.argv = ["--", "--port", "9477"]
+    print("Starting wisp server on port 9477...")
+    runpy.run_module("wisp.server", run_name = "__main__")
+threading.Thread(target=start_wisp_server).start()
+HTTPServer(("0.0.0.0", 8823), SimpleHTTPRequestHandler).serve_forever()
